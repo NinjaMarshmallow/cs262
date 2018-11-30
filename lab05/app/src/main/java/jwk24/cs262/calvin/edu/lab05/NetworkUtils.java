c4e24257cd7090ee9567fbd43dea4f4dc38659f6
NinjaMarshmallow <jwk24@students.calvin.edu>
2018-11-29 23:37:45 -0500

    Added Homework 4

--- lab05/app/src/main/java/jwk24/cs262/calvin/edu/lab05/NetworkUtils.java
+++ lab05/app/src/main/java/jwk24/cs262/calvin/edu/lab05/NetworkUtils.java
@@ -1,6 +1,8 @@
 package jwk24.cs262.calvin.edu.lab05;
 
 import android.net.Uri;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.util.Log;
 
 import java.io.BufferedReader;
@@ -9,30 +11,32 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.Base64;
 
 public class NetworkUtils {
 
     private static final String LOG_TAG = NetworkUtils.class.getSimpleName();
 
-    private static final String BOOK_BASE_URL =  "https://www.googleapis.com/books/v1/volumes?"; // Base URI for the Books API
+    private static final String BOOK_BASE_URL =  "http://cs262-teamb-fall2019.appspot.com/Game/v1/Game"; // Base URI for the Books API
     private static final String QUERY_PARAM = "q"; // Parameter for the search string
     private static final String MAX_RESULTS = "maxResults"; // Parameter that limits search results
     private static final String PRINT_TYPE = "printType";   // Parameter to filter by print type
 
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     static String getBookInfo(String queryString){
         HttpURLConnection urlConnection = null;
         BufferedReader reader = null;
         String bookJSONString = null;
         try {
             Uri builtURI = Uri.parse(BOOK_BASE_URL).buildUpon()
-                    .appendQueryParameter(QUERY_PARAM, queryString)
-                    .appendQueryParameter(MAX_RESULTS, "10")
-                    .appendQueryParameter(PRINT_TYPE, "books")
                     .build();
             URL requestURL = new URL(builtURI.toString());
             urlConnection = (HttpURLConnection) requestURL.openConnection();
             urlConnection.setRequestMethod("GET");
+            String userpass = "postgres:Quest";
+            String basicAuth = "Basic " + new String(Base64.getEncoder().encode(userpass.getBytes()));
+            urlConnection.setRequestProperty("Authorization", basicAuth);
             urlConnection.connect();
             InputStream inputStream = urlConnection.getInputStream();
             StringBuffer buffer = new StringBuffer();
@@ -69,6 +73,7 @@ public class NetworkUtils {
                 }
             }
             Log.d(LOG_TAG, bookJSONString);
+            System.out.println(bookJSONString);
             return bookJSONString;
         }
 
